50218e4673d3ac56ca9d40c7c559e46ac845c46d
caroholix <149882467+caroholix@users.noreply.github.com>
2025-12-10 21:18:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,36 +1,32 @@
 import os
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
-import json
 
-# Read JSON key from environment variable
+# Read JSON from GitHub Secret
 json_key = os.environ["GOOGLE_API_JSON"]
-json_key_file = "service_key.json"
 
-# Write JSON key to file
-with open(json_key_file, "w") as f:
+# Save JSON to file
+with open("service_key.json", "w") as f:
     f.write(json_key)
 
-# URL to index
-URL = os.environ.get("URL_TO_INDEX", "https://amosix.in/")
-
 SCOPES = ["https://www.googleapis.com/auth/indexing"]
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key_file, scopes=SCOPES)
+credentials = ServiceAccountCredentials.from_json_keyfile_name("service_key.json", SCOPES)
 http = credentials.authorize(httplib2.Http())
 
+URL = os.environ.get("URL_TO_INDEX", "https://amosix.in/")
 endpoint = "https://indexing.googleapis.com/v3/urlNotifications:publish"
 
 content = {
-  "url": URL,
-  "type": "URL_UPDATED"
+    "url": URL,
+    "type": "URL_UPDATED"
 }
 
 response, body = http.request(
     endpoint,
     method="POST",
     headers={"Content-Type": "application/json"},
-    body=json.dumps(content),
+    body=json.dumps(content)
 )
 
 print("Indexed:", URL)
